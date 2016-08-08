3237a5829d8eec63b399ffd0730bd9d4341fdd54
myzhan <scau.myzhan@gmail.com>
2016-08-08 10:24:17 +0800

    BUGFIX: implement the setUp method

--- src/main/java/com/github/myzhan/gitlab/SkippingBuildExpressionSetter.java
+++ src/main/java/com/github/myzhan/gitlab/SkippingBuildExpressionSetter.java
@@ -1,10 +1,15 @@
 package com.github.myzhan.gitlab;
 
 import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
+import hudson.model.BuildListener;
 import hudson.tasks.BuildWrapper;
 import hudson.tasks.BuildWrapperDescriptor;
 import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.io.IOException;
 
 /**
  * Created by myzhan on 16/8/5.
@@ -12,6 +17,17 @@ import org.kohsuke.stapler.DataBoundConstructor;
 public class SkippingBuildExpressionSetter extends BuildWrapper {
 
     public String skippingBuildExpression;
+
+
+    @Override
+    public Environment setUp(AbstractBuild build, Launcher launcher, BuildListener listener) throws IOException, InterruptedException {
+        return new Environment() {
+            @Override
+            public boolean tearDown(AbstractBuild build, BuildListener listener) throws IOException, InterruptedException {
+                return super.tearDown(build, listener);
+            }
+        };
+    }
 
 
     @DataBoundConstructor
